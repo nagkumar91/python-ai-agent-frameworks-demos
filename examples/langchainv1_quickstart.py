542dc7f02e0f2abf2cddc9578116989c95937316
Pamela Fox <pamela.fox@gmail.com>
2025-09-05 17:26:10 -0700

    Fix return type

--- examples/langchainv1_quickstart.py
+++ examples/langchainv1_quickstart.py
@@ -73,7 +73,7 @@ class UserContext:
 
 
 @tool
-def get_weather(city: str) -> str:  # (1)!
+def get_weather(city: str) -> str:
     """Get weather for a given city."""
     return f"It's always sunny in {city}!"
 
